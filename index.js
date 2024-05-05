a33bce9158d76f1c3fe926ee8a8134f1589b3c1c
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-05 18:51:07 -0500

    Update index.js
    defined starter code questions variable with all questions needed to be asked during readme generation.

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: 'input', name: 'Title', message: 'What is the title of your project?'},
+    {type: 'input', name: 'Description', message: 'Prove a short description of your Application.'},
+    {type: 'input', name: 'Installation', message: 'Define steps required to install your project.'},
+    {type: 'input', name: 'Usage', message: 'Provide examples of use for your project.'},
+    {type: 'input', name: 'License', message: '"MIT", "Apache", "GPL", "None"'},
+    {type: 'input', name: 'Contributing', message: 'Add contributing developers, or instructions on how they can contribute!'},
+    {type: 'input', name: 'Tests', message: 'If you are developing tests, include examples here.'},
+    {type: 'input', name: 'Email', message: 'Please enter a valid contact email here!'},
+    {type: 'input', name: 'Username', message: 'Please enter your GitHub username here!'},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
